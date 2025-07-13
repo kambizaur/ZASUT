de3420cb01c0a175901adf0a1af2a9d4f5451595
Zaur <zaur.kambi@gmail.com>
2025-07-13 16:57:01 +0500

    for merge

--- SIR_models.py
+++ SIR_models.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 def old_model(t, S, I, R, b=0.3, g=0.05):
 
@@ -23,6 +23,44 @@ def old_model(t, S, I, R, b=0.3, g=0.05):
 
     return (np.array(Slist), np.array(Ilist), np.array(Rlist))
 
+
+def new_model(t, S, I, R, b=0.3, g=0.05):
+
+    k_max = len(S)
+
+    k = np.arange(k_max)
+
+    n = S + I + R
+    
+    k_mean = (n*k).sum()
+
+    #coeff = ((n * k ** 2).sum() - k_mean) / k_mean
+    coeff = 1 #k_mean
+
+    Slist, Ilist, Rlist = [S], [I], [R]
+
+    for i in range(len(t)-1):
+        if I.sum() > 1e-6:
+            Theta = (n * k * I).sum() / k_mean
+
+            Sd = - b / coeff * k * Theta * S
+            Rd = g * I
+            Id = - Sd - Rd
+
+        
+            S_new = S + Sd * (t[i+1] - t[i])
+            I_new = I + Id * (t[i+1] - t[i])
+            R_new = R + Rd * (t[i+1] - t[i])
+        
+            S, I, R = S_new, I_new, R_new
+
+        Slist.append(S)
+        Ilist.append(I)
+        Rlist.append(R)
+
+    return (np.array(Slist), np.array(Ilist), np.array(Rlist))
+
+
 def alpha_model(t, S, I, R, b=0.3, g=0.05, alpha=1):
 
     Slist, Ilist, Rlist = [S], [I], [R]
@@ -37,38 +75,6 @@ def alpha_model(t, S, I, R, b=0.3, g=0.05, alpha=1):
             I_new = I + Id * (t[i+1] - t[i])
             R_new = R + Rd * (t[i+1] - t[i])
 
-            S, I, R = S_new, I_new, R_new
-
-        Slist.append(S)
-        Ilist.append(I)
-        Rlist.append(R)
-
-    return (np.array(Slist), np.array(Ilist), np.array(Rlist))
-
-
-
-def new_model(t, S, I, R, b=0.3, g=0.05):
-
-    k_max = len(S)
-
-    k = np.arange(k_max)
-
-    n = S + I + R
-    
-    k_mean = (n*k).sum()
-
-    Slist, Ilist, Rlist = [S], [I], [R]
-
-    for i in range(len(t)-1):
-        if I.sum() > 1e-6:
-            Sd = -k / k_mean * b * (n*k*I).sum() / k_mean * S
-            Id = k / k_mean * b * (n*k*I).sum() / k_mean * S - g * I
-            Rd = g * I
-        
-            S_new = S + Sd * (t[i+1] - t[i])
-            I_new = I + Id * (t[i+1] - t[i])
-            R_new = R + Rd * (t[i+1] - t[i])
-        
             S, I, R = S_new, I_new, R_new
 
         Slist.append(S)
